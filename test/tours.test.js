0aaf72ad52c057a0d71b44e9e1256b79bc3c7db8
sepuckett86 <sepuckett86@gmail.com>
2019-07-19 14:26:07 -0700

    add get test for tours

--- test/tours.test.js
+++ test/tours.test.js
@@ -5,6 +5,8 @@ const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 
+const Tour = require('../lib/models/Tour');
+
 describe('app routes', () => {
   beforeAll(() => {
     connect();
@@ -17,26 +19,44 @@ describe('app routes', () => {
   afterAll(() => {
     return mongoose.connection.close();
   });
-  describe('tour routes', () => {
-    it('posts a new tour', () => {
-      const tour = {
-        title: 'Mind Exploration',
-        launchDate: Date.now()
-      };
 
-      return request(app)
-        .post('/api/v1/tours')
-        .send(tour)
-        .then(res => {
-          expect(res.body).toEqual({
-            _id: expect.any(String),
-            title: 'Mind Exploration',
-            launchDate: expect.any(String),
-            activities: [],
-            stops: [],
-            __v: 0
-          });
+  it('posts a new tour', () => {
+    const tour = {
+      title: 'Mind Exploration',
+      launchDate: Date.now()
+    };
+
+    return request(app)
+      .post('/api/v1/tours')
+      .send(tour)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          title: 'Mind Exploration',
+          launchDate: expect.any(String),
+          activities: [],
+          stops: [],
+          __v: 0
         });
-    });
+      });
+  });
+
+  it('gets all tours', async() => {
+    const tour = JSON.parse(JSON.stringify(await Tour.create({
+      title: 'Mind Exploration',
+      launchDate: Date.now()
+    })));
+
+    return request(app)
+      .get('/api/v1/tours')
+      .then(res => {
+        expect(res.body).toEqual([
+          {
+            _id: tour._id,
+            title: 'Mind Exploration',
+            launchDate: expect.any(String)
+          }
+        ]);
+      });
   });
 });
